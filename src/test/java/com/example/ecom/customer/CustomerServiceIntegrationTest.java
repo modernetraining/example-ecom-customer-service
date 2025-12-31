9b6e6dc376b75ba61891011e4c171deb667765cd
Matthew Campbell <matt@moderne.io>
2025-12-31 17:15:51 -0500

    Baseline
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/com/example/ecom/customer/CustomerServiceIntegrationTest.java
+++ src/test/java/com/example/ecom/customer/CustomerServiceIntegrationTest.java
@@ -2,14 +2,12 @@ package com.example.ecom.customer;
 
 import com.example.ecom.customer.model.Customer;
 import com.example.ecom.customer.repository.CustomerRepository;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Collections;
@@ -22,7 +20,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
 public class CustomerServiceIntegrationTest {
